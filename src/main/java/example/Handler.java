5bd1f52c04e0824ef89c4e0497d1e52410fe6db4
Aakash Verma <a.v2612@gmail.com>
2020-08-14 07:10:08 -0500

    Update Handler.java

--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -28,7 +28,11 @@ public class Handler implements RequestHandler<Map, String>{
         Map queryString = (Map) params.getOrDefault("querystring"
                 , new HashMap<String, Map>());
 
-        String 7987878 = new String("Hello " + queryString.get("name"));
+        String a = new String("Hello " + queryString.get("name"));
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        while(true){
+            a.add(1);
+        }
         return response;
     }
 }
